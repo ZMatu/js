a86965b35ee486f478338911a3a990644a72bd47
Matias <78674116+ZMatu@users.noreply.github.com>
2024-03-19 18:33:53 -0300

    Acomodando el codigo

--- index.js
+++ index.js
@@ -3,50 +3,50 @@ dineroValen = prompt("Cuanto money tiene valencua?");
 dineroEze = prompt("Cuanto money tiene eze?");
 
 //GUADA
-if (dineroGuadi >= 0.8 && dineroGuadi <1.2){
-   alert("comprate el helado de agua");
+if (dineroGuadi >= 0.8 && dineroGuadi < 1.2) {
+    alert("comprate el helado de agua");
 }
-else if (dineroGuadi >= 1.6 && dineroGuadi <1.7){ 
+else if (dineroGuadi >= 1.6 && dineroGuadi < 1.7) {
     alert("comprate el helado de crema");
 }
-else if (dineroGuadi >= 1.8 && dineroGuadi <2.5){
+else if (dineroGuadi >= 1.8 && dineroGuadi < 2.5) {
     alert("comprate el helado de crema");
 }
-else if (dineroGuadi >= 3){
+else if (dineroGuadi >= 3) {
     alert("torta grido");
 }
-else{
-alert("se te perdio la billetera?");
+else {
+    alert("se te perdio la billetera?");
 }
 // VALEN
-if (dineroValen >= 0.8 && dineroValen <1.2){
+if (dineroValen >= 0.8 && dineroValen < 1.2) {
     alert("comprate el helado de agua");
- }
- else if (dineroValen >= 1.6 && dineroValen <1.7){ 
-     alert("comprate el helado de crema");
- }
- else if (dineroValen >= 1.8 && dineroValen <2.5){
-     alert("comprate el helado de crema");
- }
- else if (dineroValen >= 3){
-     alert("torta grido");
- }
- else{
- alert("se te perdio la billetera?");
- }
+}
+else if (dineroValen >= 1.6 && dineroValen < 1.7) {
+    alert("comprate el helado de crema");
+}
+else if (dineroValen >= 1.8 && dineroValen < 2.5) {
+    alert("comprate el helado de crema");
+}
+else if (dineroValen >= 3) {
+    alert("torta grido");
+}
+else {
+    alert("se te perdio la billetera?");
+}
 //EZE
- if (dineroEze >= 0.8 && dineroEze <1.2){
+if (dineroEze >= 0.8 && dineroEze < 1.2) {
     alert("comprate el helado de agua");
- }
- else if (dineroEze >= 1.6 && dineroEze <1.7){ 
-     alert("comprate el helado de crema");
- }
- else if (dineroEze >= 1.8 && dineroEze <2.5){
-     alert("comprate el helado de crema");
- }
- else if (dineroEze >= 3){
-     alert("torta grido");
- }
- else{
- alert("se te perdio la billetera?");
- }
+}
+else if (dineroEze >= 1.6 && dineroEze < 1.7) {
+    alert("comprate el helado de crema");
+}
+else if (dineroEze >= 1.8 && dineroEze < 2.5) {
+    alert("comprate el helado de crema");
+}
+else if (dineroEze >= 3) {
+    alert("torta grido");
+}
+else {
+    alert("se te perdio la billetera?");
+}
