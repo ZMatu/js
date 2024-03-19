0c670ca35a37e1d58b29664c3d12f89e21125dee
Matias <78674116+ZMatu@users.noreply.github.com>
2024-03-19 19:00:40 -0300

    fixed

--- index.js
+++ index.js
@@ -4,49 +4,49 @@ dineroEze = prompt("Cuanto money tiene eze?");
 
 //GUADA
 if (dineroGuadi >= 0.8 && dineroGuadi < 1.2) {
-    alert("comprate el helado de agua");
+    alert("comprate el helado de agua guadi");
 }
 else if (dineroGuadi >= 1.6 && dineroGuadi < 1.7) {
-    alert("comprate el helado de crema");
+    alert("comprate el helado de crema guadi"); 
 }
 else if (dineroGuadi >= 1.8 && dineroGuadi < 2.5) {
-    alert("comprate el helado de crema");
+    alert("comprate el helado de oreo guadi ");
 }
 else if (dineroGuadi >= 3) {
-    alert("torta grido");
+    alert("torta grido para guadi");
 }
 else {
-    alert("se te perdio la billetera?");
+    alert("se te perdio la billetera guadalupe?");
 }
 // VALEN
 if (dineroValen >= 0.8 && dineroValen < 1.2) {
-    alert("comprate el helado de agua");
+    alert("comprate el helado de agua valen");
 }
 else if (dineroValen >= 1.6 && dineroValen < 1.7) {
-    alert("comprate el helado de crema");
+    alert("comprate el helado de crema valen");
 }
 else if (dineroValen >= 1.8 && dineroValen < 2.5) {
-    alert("comprate el helado de crema");
+    alert("comprate el helado de oreo valen");
 }
 else if (dineroValen >= 3) {
-    alert("torta grido");
+    alert("torta grido para valen");
 }
 else {
-    alert("se te perdio la billetera?");
+    alert("se te perdio la billetera valentina?");
 }
 //EZE
 if (dineroEze >= 0.8 && dineroEze < 1.2) {
-    alert("comprate el helado de agua");
+    alert("comprate el helado de agua eze");
 }
 else if (dineroEze >= 1.6 && dineroEze < 1.7) {
-    alert("comprate el helado de crema");
+    alert("comprate el helado de crema eze");
 }
 else if (dineroEze >= 1.8 && dineroEze < 2.5) {
-    alert("comprate el helado de crema");
+    alert("comprate el helado de oreo eze");
 }
 else if (dineroEze >= 3) {
-    alert("torta grido");
+    alert("torta grido para eze");
 }
 else {
-    alert("se te perdio la billetera?");
+    alert("se te perdio la billetera ezequiel?");
 }
